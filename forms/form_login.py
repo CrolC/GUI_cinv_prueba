b67618bef06d191513ecb94603ce93d6b2883832
CSR <salazarruizcarolina@gmail.com>
2025-03-27 14:19:09 -0600

    modificar comentarios

---
+++ forms/form_login.py
@@ -0,0 +1,142 @@
+import customtkinter as ctk
+from tkinter import messagebox
+import sqlite3
+import sys
+sys.path.append('d:/Python_Proyectos/INTER_C3')
+import util.generic as utl
+from forms.form_master import MasterPanel
+
+# Función para crear la base de datos y la tabla de usuarios
+def inicializar_base_datos():
+    conn = sqlite3.connect("usuarios.db")
+    cursor = conn.cursor()
+
+    # Crear la tabla si no existe (SQL)
+    cursor.execute('''CREATE TABLE IF NOT EXISTS usuarios (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        usuario TEXT NOT NULL UNIQUE,
+                        password TEXT NOT NULL)''')
+
+    conn.commit()
+    conn.close()
+
+# LOGIN
+class App:
+
+# En el código de la clase App, dentro de la función `verificar`:
+
+    def verificar(self):
+        usu = self.usuario.get()
+        password = self.password.get()
+
+        conn = sqlite3.connect("usuarios.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE usuario=? AND password=?", (usu, password))
+        resultado = cursor.fetchone()
+        conn.close()
+
+        if resultado:
+            self.ventana.withdraw()  # Ocultar la ventana en lugar de destruirla
+            master_panel = MasterPanel()  
+            master_panel.mainloop()  # Iniciar la nueva ventana
+            self.ventana.destroy()  # Cerrar la ventana de login después de que MasterPanel termine
+        else:
+            messagebox.showerror(message="Usuario o contraseña incorrectos", title="Error")
+
+
+    #def mostrar_panel_maestro(self):
+        #master_panel = MasterPanel()  # Instancia de la ventana principal
+        #master_panel.mainloop()  # Iniciar el mainloop en la ventana de MasterPanel
+    def mostrar_panel_maestro(self):
+        master_panel = MasterPanel()
+        master_panel.mainloop()  # Asegurar que la ventana principal se ejecute
+
+    def registrar_usuario(self):
+        usu = self.usuario_registro.get()
+        password = self.password_registro.get()
+
+        if not usu or not password:
+            messagebox.showwarning("Error", "Todos los campos son obligatorios")
+            return
+
+        conn = sqlite3.connect("usuarios.db")
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM usuarios WHERE usuario=?", (usu,))
+        if cursor.fetchone():
+            messagebox.showwarning("Error", "El usuario ya existe")
+        else:
+            cursor.execute("INSERT INTO usuarios (usuario, password) VALUES (?, ?)", (usu, password))
+            conn.commit()
+            messagebox.showinfo("Éxito", "Usuario registrado exitosamente")
+            self.ventana_registro.destroy()
+
+        conn.close()
+
+    def mostrar_ventana_registro(self):
+        self.ventana_registro = ctk.CTkToplevel(self.ventana)
+        self.ventana_registro.title("Registrar usuario")
+        self.ventana_registro.geometry("400x300")
+
+        self.usuario_registro = ctk.CTkEntry(self.ventana_registro, placeholder_text="Usuario")
+        self.usuario_registro.pack(pady=10)
+        self.password_registro = ctk.CTkEntry(self.ventana_registro, placeholder_text="Contraseña", show="*")
+        self.password_registro.pack(pady=10)
+
+        registrar_btn = ctk.CTkButton(self.ventana_registro, text="Registrar", command=self.registrar_usuario, fg_color="#06918A")
+        registrar_btn.pack(pady=20)
+
+    def __init__(self):
+        inicializar_base_datos()
+
+        ctk.set_appearance_mode("light")
+        ctk.set_default_color_theme("green")
+
+        self.ventana = ctk.CTk()
+        self.ventana.title('Inicio de sesión')
+        self.ventana.geometry('800x500')
+        self.ventana.resizable(width=False, height=False)
+        utl.centrar_ventana(self.ventana, 800, 500)
+
+        # Frame del logo (lado izquierdo)
+        frame_logo = ctk.CTkFrame(self.ventana, width=400, corner_radius=0, fg_color="#06918A")  # Color personalizado
+        frame_logo.pack(side="left", expand=True, fill="both")
+
+        # Lectura del logo (puedes cambiar el logo si es necesario)
+        logo = utl.leer_imagen("d:/Python_Proyectos/INTER_C3/imagenes/logo.png", (200, 200))
+        label_logo = ctk.CTkLabel(frame_logo, image=logo, text="")
+        label_logo.place(relx=0.5, rely=0.5, anchor="center")  # Centrar el logo
+
+        # Frame del formulario (lado derecho)
+        frame_form = ctk.CTkFrame(self.ventana, corner_radius=0)
+        frame_form.pack(side="right", expand=True, fill="both")
+
+        # Título del formulario (centrado horizontalmente)
+        title = ctk.CTkLabel(frame_form, 
+                            text="Sistema de Crecimiento\npor Epitaxia de\nHaces Moleculares", 
+                            font=ctk.CTkFont(size=25, weight="bold"), 
+                            justify="center")
+        title.pack(pady=30, padx=20, anchor="n")  # Centrado horizontal con "anchor" y espacio arriba con "pady"
+
+        # Campo de usuario
+        self.usuario = ctk.CTkEntry(frame_form, placeholder_text="Usuario", font=ctk.CTkFont(size=14))
+        self.usuario.pack(pady=10, padx=20, fill="x")
+
+        # Campo de contraseña
+        self.password = ctk.CTkEntry(frame_form, placeholder_text="Contraseña", font=ctk.CTkFont(size=14), show="*")
+        self.password.pack(pady=10, padx=20, fill="x")
+
+        # Botón de inicio de sesión
+        inicio = ctk.CTkButton(frame_form, text="Iniciar sesión", font=ctk.CTkFont(size=15, weight="bold"),
+                            command=self.verificar, fg_color="#06918A")
+        inicio.pack(pady=20, padx=20, fill="x")
+
+        # Botón para abrir ventana de registro
+        registro = ctk.CTkButton(frame_form, text="Registrarse", font=ctk.CTkFont(size=15), 
+                                command=self.mostrar_ventana_registro, fg_color="#06918A")
+        registro.pack(pady=10, padx=20, fill="x")
+
+
+        self.ventana.mainloop()
+
+if __name__ == "__main__":
+    App()
---
+++ forms/form_master.py
@@ -0,0 +1,123 @@
+import customtkinter as ctk
+import sys
+#from tkinter import font  # Importar tipografías si las necesitas
+sys.path.append('d:/Python_Proyectos/INTER_C3')
+import util.generic as utl
+from forms.form_nuevoproceso import FormNuevoProceso
+from forms.form_paneldecontrol import FormPaneldeControl
+
+COLOR_BARRA_SUPERIOR = "#1a1e23"  # Gris oscuro suave
+COLOR_MENU_LATERAL = "#1f3334"  # Verde azulado oscuro
+COLOR_CUERPO_PRINCIPAL = "#f4f8f7"  # Blanco roto con toque gris
+COLOR_MENU_CURSOR_ENCIMA = "#18a9b1"  # Azul verdoso claro
+
+
+class MasterPanel(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.config_window()  # Crear la ventana primero
+        self.logo = utl.leer_imagen("d:/Python_Proyectos/INTER_C3/imagenes/logocinves_bco.png", size=(400, 136))  # Cargar la imagen después
+        self.perfil = utl.leer_imagen("d:/Python_Proyectos/INTER_C3/imagenes/Perfil.png", size=(100, 100))
+        self.predeterminada = utl.leer_imagen("d:/Python_Proyectos/INTER_C3/imagenes/predeterm.png", size=(300, 100))
+        self.paneles()
+        self.controles_barra_superior()
+        self.controles_menu_lateral()
+        self.controles_cuerpo()
+
+
+    def config_window(self):
+        self.title('Cinvestav')
+        self.iconbitmap("d:/Python_Proyectos/INTER_C3/imagenes/logo.ico")
+        w, h = 1024, 600        
+        utl.centrar_ventana(self, w, h)  
+
+    def paneles(self):
+        # Crear paneles: barra superior, menú lateral y cuerpo principal
+        self.barra_superior = ctk.CTkFrame(self, fg_color=COLOR_BARRA_SUPERIOR, height=50)
+        self.barra_superior.pack(side=ctk.TOP, fill='both')
+
+        self.menu_lateral = ctk.CTkFrame(self, fg_color=COLOR_MENU_LATERAL, width=150)
+        self.menu_lateral.pack(side=ctk.LEFT, fill='both', expand=False)
+
+        self.cuerpo_principal = ctk.CTkFrame(self, fg_color=COLOR_CUERPO_PRINCIPAL)
+        self.cuerpo_principal.pack(side=ctk.RIGHT, fill='both', expand=True)
+
+    def controles_barra_superior(self):
+        font_awesome = ctk.CTkFont(family="FontAwesome", size=12)
+
+        # Etiqueta de título
+        self.labelTitulo = ctk.CTkLabel(
+            self.barra_superior, text="Sistema MBE",
+            text_color="white", font=ctk.CTkFont(family="Roboto", size=15))
+        self.labelTitulo.pack(side=ctk.LEFT, padx=10, pady=10)
+
+        # Botón del menú lateral
+        self.buttonMenuLateral = ctk.CTkButton(
+            self.barra_superior, text="\uf022", font=font_awesome,
+            command=self.toggle_panel, fg_color=COLOR_BARRA_SUPERIOR, text_color="white")
+        self.buttonMenuLateral.pack(side=ctk.LEFT, padx=5)
+
+        # Etiqueta de información
+        self.labelInfo = ctk.CTkLabel(
+            self.barra_superior, text="Crecimiento por Epitaxia de Haces Moleculares",
+            text_color="white", font=ctk.CTkFont(family="Roboto", size=10))
+        self.labelInfo.pack(side=ctk.RIGHT, padx=10, pady=10)
+
+    def controles_menu_lateral(self):
+        ancho_menu = 20
+        alto_menu = 2
+
+        font_awesome = ctk.CTkFont(family="FontAwesome", size=15)
+
+        # Etiqueta de perfil
+        if self.perfil:  # Check if the image was loaded successfully
+            self.labelPerfil = ctk.CTkLabel(self.menu_lateral, image=self.perfil, text="", fg_color=COLOR_MENU_LATERAL)
+        else:
+            self.labelPerfil = ctk.CTkLabel(self.menu_lateral, text="Perfil", fg_color=COLOR_MENU_LATERAL)
+        self.labelPerfil.pack(side=ctk.TOP, pady=10)
+
+        # Botones del menú lateral
+        buttons_info = [ 
+            ("Nuevo proceso", "\uf144", self.abrir_nuevoproceso),  # 3 elements
+            ("Historial", "\uf07c", None),  # Add None for missing command
+            ("Diagnóstico", "\uf044", None), 
+            ("Panel de Control", "\uf080", self.abrir_paneldecontrol), 
+            ("Monitoreo del Proceso", "\uf017", None) 
+        ]
+
+        for text, icon, command in buttons_info:  # Unpack 3 elements
+            button = ctk.CTkButton(
+                self.menu_lateral, text=f"  {icon}    {text}",
+                font=font_awesome, anchor="w",
+                fg_color=COLOR_MENU_LATERAL, text_color="white",
+                hover_color=COLOR_MENU_CURSOR_ENCIMA,
+                command=command  # Use the command if provided
+            )
+            button.pack(side=ctk.TOP, fill="x", padx=5, pady=5)
+
+    def controles_cuerpo(self):
+        # Imagen en el cuerpo principal
+        label = ctk.CTkLabel(self.cuerpo_principal, image=self.logo, text="", fg_color=COLOR_CUERPO_PRINCIPAL)
+        label.place(x=0, y=0, relwidth=1, relheight=1)
+
+    def toggle_panel(self):
+        if self.menu_lateral.winfo_ismapped():
+            self.menu_lateral.pack_forget()
+        else:
+            self.menu_lateral.pack(side=ctk.LEFT, fill='both', expand=False)  # Restore original layout
+
+    def abrir_nuevoproceso(self):
+        self.limpiar_panel(self.cuerpo_principal)
+        FormNuevoProceso(self.cuerpo_principal, self.predeterminada)
+
+    def abrir_paneldecontrol(self):
+        self.limpiar_panel(self.cuerpo_principal)
+        FormPaneldeControl(self.cuerpo_principal, self.predeterminada)
+
+    def limpiar_panel(self, panel):
+        for widget in panel.winfo_children():
+            widget.destroy()
+
+if __name__ == "__main__":
+    app = MasterPanel()
+    app.mainloop()
