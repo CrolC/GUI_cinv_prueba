e70ed3f9d5972caea205581edc5a51f8551d8a0f
CSR <salazarruizcarolina@gmail.com>
2025-04-06 19:19:27 -0600

    No funcional (En construccion)

--- forms/form_monitoreo.py
+++ forms/form_monitoreo.py
@@ -1,19 +1,21 @@
 import customtkinter as ctk
 import sys
-from PIL import Image, ImageTk
 sys.path.append('d:/Python_Proyectos/INTER_C3')
 import util.generic as utl
 
-
-class FormMonitoreo(ctk.CTk):
+class FormMonitoreo(ctk.CTkFrame):
     def __init__(self, panel_principal, predeterminada):
         super().__init__(panel_principal)
-        self.predeterminada = predeterminada
+        
+        self.grid_rowconfigure(0, weight=1) 
+        self.grid_columnconfigure(0, weight=1)
+        
+        self.frame_construccion = ctk.CTkFrame(self, width=400, height=200)  # Ajustar tamaño aquí
+        self.frame_construccion.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")  
+        
+        self.label_construccion = ctk.CTkLabel(self.frame_construccion, 
+                                                text='Panel en construcción', 
+                                                font=ctk.CTkFont(size=20, weight="bold"))
+        self.label_construccion.pack(fill='x', padx=5, pady=20)
 
-       
-        self.frame_construccion = ctk.CTkFrame(self)
-        self.frame_construccion.pack(pady=10, padx=10, fill='both', expand=True)
-
-        ctk.CTkLabel(self.frame_construccion, text='Panel en construcción', font=ctk.CTkFont(size=20, weight="bold")).pack(fill='x', padx=5, pady=20)
-
-
+        self.grid(row=0, column=0, sticky="nsew")  
